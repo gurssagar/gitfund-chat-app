15d01ae0d4e6e1ca92607b9830ff248337ecb8fb
gurssagar <123202956+gurssagar@users.noreply.github.com>
2025-05-13 18:58:11 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ const app = express();
 
 // Add CORS middleware before other routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*:');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
